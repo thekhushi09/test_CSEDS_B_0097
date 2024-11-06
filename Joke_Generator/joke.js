30dc233ffd9b93418b96c74f2665492e6813c02a
thekhushi09 <thekhushitomar10@gmail.com>
2024-11-06 15:05:22 +0530

    new

---
+++ Joke_Generator/joke.js
@@ -0,0 +1,29 @@
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
+const memeImage=document.querySelector(".meme-generator img");
+const memeTitle=document.querySelector(".meme-generator .meme-title");
+const memeAuthor=document.querySelector(".meme-generator .meme-author");
+
+
+const result=document.querySelector(".result");
+
+const updateDetails = (category,type,delivery) => {
+    memeImage.setAttribute("src",category);
+    memeTitle.innerHTML = type;
+    memeAuthor.innerHTML = delivery;
+
+
+}
+// https://v2.jokeapi.dev/joke/Any
+const generateJoke=()=>{
+    fetch("https://v2.jokeapi.dev/joke/Any")               
+    .then( (response) => response.json())
+    .then(data => {
+            updateDetails(data.category, data.type, data.delivery)
+
+        });
+    
+
+};
+generateMemeBtn.addEventListener("click",generateJoke);
+
+result.innerHTML=generateJoke();
